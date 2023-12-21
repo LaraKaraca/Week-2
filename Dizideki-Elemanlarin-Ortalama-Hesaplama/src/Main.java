eef49d6703077a9d2b86a61c10e8c7cd21292324
Lara Karaca <152811987+LaraKaraca@users.noreply.github.com>
2023-12-22 00:45:27 +0300

    Harmonik Hesaplandı

---
+++ Dizideki-Elemanlarin-Ortalama-Hesaplama/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        harmonicMean(numbers);
+    }
+
+    public static void harmonicMean(int[] arr){
+        double harmonicMean = 0;
+
+        for (int i: arr) {
+            harmonicMean += 1.0 / i;
+        }
+        System.out.println("Dizinin harmonik ortalaması: " + arr.length / harmonicMean);
+    }
+}
